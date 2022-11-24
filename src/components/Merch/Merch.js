a70b6b2dc6cf5846aef78a95539013946aee35d6
grahamjantz <grahammrjantz@gmail.com>
2022-11-24 15:15:12 -0700

    further work on merch page with printify api

--- src/components/Merch/Merch.js
+++ src/components/Merch/Merch.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import * as Scroll from 'react-scroll'
 import './Merch.css'
 
@@ -9,39 +9,46 @@ import merchItems from './merchObject.js'
 
 const Merch = ({ name }) => {
 
+  const [fetchedMerchItems, setFetchedMerchItems] = useState([])
+
+  const baseUrl = 'https://api.printify.com/v1/'
+  const productsReq = 'shops/{shop_id}/products.json'
+  const apiToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIzN2Q0YmQzMDM1ZmUxMWU5YTgwM2FiN2VlYjNjY2M5NyIsImp0aSI6Ijk1MmI4YTcxOWM1YTY1MDBjYjVhY2Y4ZDQyMjBmNzJhNTQ2MjliMjE5ZWE4ODMzNGQ3N2Y1YmJiZmY5NzcxMTMwYjZlMjJiMjU4ZmIzZTFkIiwiaWF0IjoxNjY5MzE0OTgwLjg2NjMyLCJuYmYiOjE2NjkzMTQ5ODAuODY2MzIyLCJleHAiOjE3MDA4NTA5ODAuODMwNTg2LCJzdWIiOiI4ODAzMTk5Iiwic2NvcGVzIjpbInNob3BzLm1hbmFnZSIsInNob3BzLnJlYWQiLCJjYXRhbG9nLnJlYWQiLCJvcmRlcnMucmVhZCIsIm9yZGVycy53cml0ZSIsInByb2R1Y3RzLnJlYWQiLCJwcm9kdWN0cy53cml0ZSIsIndlYmhvb2tzLnJlYWQiLCJ3ZWJob29rcy53cml0ZSIsInVwbG9hZHMucmVhZCIsInVwbG9hZHMud3JpdGUiLCJwcmludF9wcm92aWRlcnMucmVhZCJdfQ.AACGGiP1Fq8Zzus6Th1k3gH7Mb9fgGg1ENSpMXTr3VIQeAl3laAaocKmKrvU8RWzYhPP8RfPs5814FBu5mw'
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(`https://api.printify.com/v1/shops.json`, {
+        method: 'GET',
+        mode: 'no-cors',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer: ${apiToken}`
+        }
+      })
+      const data = res.json()
+      console.log(data)
+    } 
+    fetchData()
+  }, [])
+
   const Element = Scroll.Element;
-  
-  const mapItemImages = (imageArr) => {
-    imageArr.map((image) => {
-      if (image.is_default === true) {
-        console.log(image.src)
-        return (
-          <img alt='' className='merch-preview-image' src={image.src}/>
-        )
-      } else {
-        return ''
-      }
-    })
-  }
 
   return (
-    <Element name={name}>
+    <Element name={name} className='merch-section'>
       <div className='red-transition'>
           <h3 className='home-about-us-title'>MERCH</h3>
           <img src={redTransition} alt='' />
       </div>
       {
         merchItems.map((item) => {
+          const sortedImagesArr = item.images.sort((a, b) => Number(b.is_default) - Number(a.is_default))
           return (
             <div key={item.id}>
-              {/* <img src={item.images.map((img) => {
-                if (img.is_default === true) {
-                  return img.src
-                }
-              })} alt=''/> */}
-              {/* <img src='https://res.cloudinary.com/grahamjantzcloudinary/image/upload/v1669246001/3YLExclusiveSpacemanTee_ljwuz0.png' alt=''/> */}
-              {mapItemImages(item.images)}
-              <h4>{item.title}</h4>
+              <img src={sortedImagesArr[0].src} alt='' className='merch-preview-image'/>
+              <div className='merch-preview-info'>
+                <h4>{item.title}</h4>
+                <h4>${item.variants[0].price}</h4>
+              </div>
             </div>
           )
         })
